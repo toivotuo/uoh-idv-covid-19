299a1ebfe97c42728d4b1f9631e5734551b6429f
Tuomas Toivonen <toivotuo@kasvua.org>
2020-04-20 23:43:32 +0300

    Add a bit of Django hacking on the 'coviz' app

--- covid_explorer/coviz/views.py
+++ covid_explorer/coviz/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+#def index(request):
+#    context = {"place": "World"}
+#    return render(request, "index.html", context)
+
+def index(request):
+    import pandas as pd
+
+    datasource = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+    df = pd.read_csv(datasource)
+
+    df = df.drop(columns=["Lat", "Long"])  # clean out spatial location columns we don't need
+    df = df[df['Province/State'].isna()].drop(columns=['Province/State'])  # FIXME: Cleaning out countries with regional data; removes e.g. Canada and Australia
+    df = df.set_index('Country/Region')  # use the country as the index to make transpose work nice
+    df = df.transpose()  # transpose rows/columns to give nicely plottable data
+    df.index = pd.to_datetime(df.index)  # make the index proper Python 'datetime' for nicer plotting
+    df.columns.name = 'Country'  # make the index name nicer
+
+    data = df[['Germany', 'Italy', 'France', 'Spain']]  # select the country subset
+
+    return HttpResponse(data.to_html())
