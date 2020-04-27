cfa2a9e2af9f4fcd7e8c156e9a9ed09436084f02
Tuomas Toivonen <toivotuo@kasvua.org>
2020-04-27 22:14:26 +0300

    Port basic COVID-19 choropleth to the Django app

--- covid_explorer/coviz/views.py
+++ covid_explorer/coviz/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 #    return render(request, "index.html", context)
 
 def index(request):
+    """ Dummy index page to have something actually loading. """
     import pandas as pd
 
     datasource = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
@@ -21,3 +22,62 @@ def index(request):
     data = df[['Germany', 'Italy', 'France', 'Spain']]  # select the country subset
 
     return HttpResponse(data.to_html())
+
+
+def map(request):
+    """Render a choropleth (coloured map) of COVID-19 cases."""
+    import pandas as pd
+    import folium
+    import pycountry
+
+    # FIXME: Using rather the ECDC data would be better. Less
+    # wrangling needed than with the JHU data.
+
+    # First, fetch and wrangle the relevant data.
+
+    country_geo = '/tmp/world-countries.json'
+
+    # FIXME: Dumb to read the latest data directly from GitHub on
+    # every request.
+
+    datasource = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+    data = pd.read_csv(datasource)
+
+    data = data.drop(columns=["Lat", "Long"])  # clean out spatial location columns we don't need
+    # FIXME: Cleaning out countries with regional data; removes e.g. Canada and Australia
+    data = data[data['Province/State'].isna()].drop(columns=['Province/State'])
+
+    data = data.rename(columns={"Country/Region": "Country"})
+
+    # Convert the country names
+    def rebase(country_name):
+        country = pycountry.countries.get(name=country_name)
+        if not country:
+            return 'N/A'  # FIXME
+        return country.alpha_3
+
+    data.Country = data.Country.apply(rebase)
+
+    # Second, generate and render the map.
+
+    map = folium.Map(
+        location=[45, 0],
+        zoom_start=3,
+    )
+
+    date = "4/26/20"  # FIXME: Make data selection dynamic in the web app.
+
+    folium.Choropleth(
+        geo_data=country_geo,
+        data=data,
+        columns=['Country', date],
+        key_on='feature.id',
+        fill_color='YlGnBu',
+        fill_opacity=0.7,
+        line_opacity=0.2,
+        legend_name="Cases of COVID-19",
+    ).add_to(map)
+
+    # FIXME: The map should be rendered in an iframe.
+
+    return HttpResponse(map.get_root().render())
